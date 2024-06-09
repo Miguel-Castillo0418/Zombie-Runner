a218b25d8403aee018b0dc33a5b57617849a7a4b
OsirisCastro <osirisemcastro@gmail.com>
2024-06-09 12:07:01 -0400

    Wave countdown and lenght Initialized

---
+++ Unity Project.unity/port-Project/Assets/Script/Wave.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+
+    [SerializeField] private float countdown;
+    [SerializeField] moreWaves[] waves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            SpawnWave();
+        }
+    }
+
+    private void SpawnWave()
+    {
+
+    }
+}
+
+[System.Serializable]
+
+public class moreWaves
+{
+    public enemyAI[] enemies;
+}
