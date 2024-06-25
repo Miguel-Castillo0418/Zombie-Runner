880f4c29f9ab89ae5b6b70b4eadd1a662dbf1ca7
Jshaun Hookumchand <Hookc123@hotmail.com>
2024-06-24 21:30:24 -0400

    Update EnemyAI.cs
    KnockBackupdates

--- Unity Project.unity/port-Project/Assets/Scripts/EnemyAI.cs
+++ Unity Project.unity/port-Project/Assets/Scripts/EnemyAI.cs
@@ -117,7 +117,7 @@ public class EnemyAI : MonoBehaviour, IDamage
     void knockback()
     {
         int force = lvl * damage * 10; // Adjust this force value as needed
-        float knockbackDuration = 0.3f; // Adjust the duration of knockback
+        float knockbackDuration = 0.5f; // Adjust the duration of knockback
 
         Vector3 knockbackDirection = (gameManager.instance.player.transform.position - transform.position).normalized;
         Vector3 targetPosition = gameManager.instance.player.transform.position + knockbackDirection * 3f; // Adjust the distance of knockback
@@ -137,10 +137,5 @@ public class EnemyAI : MonoBehaviour, IDamage
             timer += Time.deltaTime;
             yield return null;
         }
-
-        // Ensure the player reaches the exact target position
-        playerTransform.position = targetPosition;
-        Rigidbody playerRigidbody = gameManager.instance.player.GetComponent<Rigidbody>();
-        playerRigidbody.angularVelocity = Vector3.zero;
     }
 }
