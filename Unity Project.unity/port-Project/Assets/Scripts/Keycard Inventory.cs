2b23ee83cdb7b3dc9ffe2b98dff47974c3505ee4
Dominick Gabriel <155986881+DGabriel-1@users.noreply.github.com>
2024-07-13 19:59:09 -0400

    Door/key and pipe Scripts

---
+++ Unity Project.unity/port-Project/Assets/Scripts/Keycard Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KeyCards
+{
+    public class KeycardInventory : MonoBehaviour
+    {
+    public bool hasKeyCard = false;
+    }
+
+}
